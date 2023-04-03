a4efeecb406f14f499815556cf09806465ad265a
Fatchul Bari Hikmawan <wawan@kyodo-i.com>
2023-04-03 15:18:49 +0700

    add bootstrap compilation

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import './bootstrap';
 import '../css/app.css';
+import * as bootstrap from 'bootstrap/dist/js/bootstrap';
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,10 @@
 
         <!-- Scripts -->
         @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
+        @vite([
+            'resources/sass/bs_custom.scss',
+            'resources/js/app.js',
+            "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
