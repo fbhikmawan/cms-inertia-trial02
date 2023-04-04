9266d33685f98eea18386b365f71bb3fa8dad832
Fatchul Bari Hikmawan <wawan@kyodo-i.com>
2023-04-04 14:16:42 +0700

    try this one

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,7 +20,7 @@
 
         <!-- JS ON HEAD -->
         <!-- Theme Config Js -->
-        <script src="{{ asset('js/hyper-config.js') }}"></script>
+        <script src="{{ asset('js/hyper-config.js') }}" defer></script>
         @routes
         @vite([
             'resources/js/app.js',
@@ -28,27 +28,27 @@
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
+        @inertia
+
         <!-- Theme Settings -->
         @include('layouts.theme.hyper-saas')
 
         <!-- JS -->
         <!-- Vendor js -->
-        <script src="{{ asset('js/vendor.min.js') }}"></script>
+        <script src="{{ asset('js/vendor.min.js') }}" defer></script>
         <!-- Daterangepicker js -->
-        <script src="{{ asset('plugins/daterangepicker/moment.min.js') }}"></script>
-        <script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
+        <script src="{{ asset('plugins/daterangepicker/moment.min.js') }}" defer></script>
+        <script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}" defer></script>
         <!-- Apex Charts js -->
-        <script src="{{ asset('plugins/apexcharts/apexcharts.min.js') }}"></script>
+        <script src="{{ asset('plugins/apexcharts/apexcharts.min.js') }}" defer></script>
         <!-- Vector Map js -->
-        <script src="{{ asset('plugins/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
-        <script src="{{ asset('plugins/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js') }}"></script>
+        <script src="{{ asset('plugins/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js') }}" defer></script>
+        <script src="{{ asset('plugins/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js') }}" defer></script>
         <!-- App js -->
-        <script src="{{ asset('js/app.min.js') }}"></script>
+        <script src="{{ asset('js/app.min.js') }}" defer></script>
         <!-- Dashboard App js -->
-        <script src="{{ asset('js/pages/demo.dashboard.js') }}"></script>
+        <script src="{{ asset('js/pages/demo.dashboard.js') }}" defer></script>
         <!-- Main Js -->
-        <script src="{{ asset('js/main.js') }}"></script>
-
-        @inertia
+        <script src="{{ asset('js/main.js') }}" defer></script>
     </body>
 </html>
